ef4435dc85b4d430f32ba971d307beb024f66998
CharlesScrivens <cscrivens@horizon.csueastbay.edu>
2024-03-01 17:15:46 -0800

    more dependecies, to restart server on save

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req,res) => {
+    // root route http://localhost.com/11337
+    res.send("hello world!");ç
+});
+
 app.listen(11337, () => console.log("Server Listening on port 11337"));
